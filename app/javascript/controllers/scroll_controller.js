b412ef3e28185261264dcf4046cda9735732ad51
Laura Jaime <laurajaimev8@gmail.com>
2022-09-30 22:06:45 +0200

    Use Stimulus for reset inputs and scroll

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  // On start
+  connect() {
+    const messages = document.getElementById("messages")
+    messages.addEventListener("DOMNodeInserted", this.resetScroll)
+    this.resetScroll(messages)
+  }
+
+  resetScroll() {
+    messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+  }
+}
